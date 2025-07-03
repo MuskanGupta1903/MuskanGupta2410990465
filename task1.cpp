0995d675da30089ccaaa1ac734def152b470d4c0
Muskan Gupta <muskangupta24559@gmail.com>
2025-07-03 10:19:52 +0530

    Update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 using namespace std;
+/*int main(){
+cout<<"HELLO WORLD"<<endl;
+return 0;} */
+
 int main(){
-cout<<"ELON MUSK"<<endl;
-return 0;}
+	int x , y;
+	
+	cout<<"ENTER A NUMBER :" <<endl;
+	cin>> x;
+	cout<<"ENTER ANOTHER NUMBER :"<<endl;
+	cin>> y;
+	cout<<"PRODUCT :" << x*y <<endl;
+	return 0;
+}
+
+
+
+
+  
