347e0078891d5fd606ae3604c133620c0eac1df0
Muskan Gupta <muskangupta24559@gmail.com>
2025-07-03 10:21:06 +0530

    Update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -3,3 +3,15 @@ using namespace std;
 int main(){
 cout<<"ELON MUSK"<<endl;
 return 0;}
+
+int main(){
+	int x , y;
+	
+	cout<<"ENTER A NUMBER :" <<endl;
+	cin>> x;
+	cout<<"ENTER ANOTHER NUMBER :"<<endl;
+	cin>> y;
+	cout<<"PRODUCT :" << x*y <<endl;
+	return 0;
+}
+
