5d302e6cd8122130d380912d2e1f74529ab7c87d
Muskan Gupta <muskangupta24559@gmail.com>
2025-07-09 20:03:26 +0530

    Create patterns.cpp

---
+++ cpp/patterns.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std; //butterfly pattern
+/*int main(){
+	int n ;
+	cout<<"ENTER NUMBER OF ROWS :";
+	cin>>n;
+	for(int i = 1 ; i<= n ;i++){
+		for(int j = 1 ; j<= i ; j++){
+			cout<<"*";
+		}
+		
+		
+	for(int j = 1 ; j <= 2*(n-i) ; j++) {
+			cout<<" ";
+		}
+		
+		for(int j = 1 ; j<= i ; j++){
+			cout<<"*";
+		}
+		cout<<endl;
+}
+
+
+for(int i = n ; i>= 1 ; i--){
+	for(int j = 1 ; j<=n ; j++){
+		cout<<"*";
+	}
+	for(int j = 1 ; j <= 2*(n-i) ; j++) {
+			cout<<" ";
+		}
+		
+		for(int j = 1 ; j<= i ; j++){
+			cout<<"*";
+		}
+		cout<<endl;
+}
+	
+} */
+
+
+
